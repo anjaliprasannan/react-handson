d7732c48e9c16007ec20bde1be1f6049d949ba62
@AnjaliPrasannan <anjali.prasannan@axelerant.com>
2024-10-22 17:23:40 +0530

    1046 - User Card

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,5 +1,25 @@
+import user from "./user.json";
+import "./user.css";
+import { UserCard } from "./Usercard";
+import { UsercardClass } from "./UsercardClass";
+
 function App() {
-  return "Hi";
+  return (
+    <>
+      <UserCard
+        name={user.name}
+        phoneNumber={user.phoneNumber}
+        age={user.age}
+        address={user.address}
+      />
+      <UsercardClass
+        name={user.name}
+        phoneNumber={user.phoneNumber}
+        age={user.age}
+        address={user.address}
+      />
+    </>
+  );
 }
 
 export default App;
